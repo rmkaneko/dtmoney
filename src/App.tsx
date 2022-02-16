e0335a7022eb7be47810aed3e4436850dc39a5f7
Roberto M. Kaneko <roberto_kaneko@hotmail.com>
2022-02-15 21:44:56 -0300

    Feat - implementing context API in React

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import Modal from 'react-modal';
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { NewTransactionModal } from './components/NewTransactionModal';
+import { TransactionsContext } from './TransactionsContext';
 
 import { GlobalStyle } from "./styles/global";
+
 
 Modal.setAppElement('#root');
 
@@ -20,7 +22,7 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsContext.Provider value={[]}>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
 
@@ -29,7 +31,7 @@ export function App() {
          onRequestClose={handleCloseNewTransactionModal}
       />
       <GlobalStyle />
-    </>
+    </TransactionsContext.Provider>
   );
 }
 
